4ebc1d98418a8854bb0784ec27b94234d5a8064c
waffleboi9000 <wafflebomb9000@gmail.com>
2023-08-28 22:23:11 -0400

    math test

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,31 @@ fn main() {
     }
 
     println!("The value of x is: {x}");
+    
+    // testing math.
+    
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("{} {}", "Sum is: ", {sum});
+    println!("{} {}", "Difference is:", {difference});
+    println!("{} {}", "Product is:",{product});
+    println!("{} {}", "Quotient is:",{quotient});
+    println!("{} {}", "Truncated is:",{truncated});
+    println!("{} {}", "Remainder is:",{remainder});
+
+
 }
