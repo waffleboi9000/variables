55cfde75163dfcf0489ec3702943b58b5608b63b
waffleboi9000 <wafflebomb9000@gmail.com>
2023-08-29 13:18:20 -0400

    testing array stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     let x = 5;
 
@@ -34,6 +35,44 @@ fn main() {
     println!("{} {}", "Quotient is:",{quotient});
     println!("{} {}", "Truncated is:",{truncated});
     println!("{} {}", "Remainder is:",{remainder});
+    
+    //let c = 'z';
+    //let z: char = 'ℤ'; // with explicit type annotation
+    //let heart_eyed_cat = '😻';
+    
+    //let x: (i32, f64, u8) = (500, 6.4, 1);
+    
+    //let five_hundred = x.0;
 
+    //let six_point_four = x.1;
+
+    //let one = x.2;
+
+    //let months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
+
+    //let array1: [i32; 5] = [1,2,3,4,5];
+
+    //let first_of_array1 = array1[0];
+
+    println!("Please enter an array index.");
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 
 }
